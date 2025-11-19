b4a7b486cc0964425b9fb409e6dcd365a651c0b5
carlosparra1998 <cf.pg@hotmail.com>
2025-11-19 16:11:27 +0100

    base project

---
+++ full_layout/bin/full_layout.dart
@@ -0,0 +1,5 @@
+import 'package:full_layout/full_layout.dart' as full_layout;
+
+void main(List<String> arguments) {
+  print('Hello world: ${full_layout.calculate()}!');
+}
---
+++ full_layout/test/full_layout_test.dart
@@ -0,0 +1,8 @@
+import 'package:full_layout/full_layout.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
